db44b00a512213b7517332eacd40089bf5be7584
Martin Lindhe <martin@ubique.se>
2013-09-08 13:41:38 +0200

    RollingParticleMap now working

--- punku/PerlinNoise/RollingParticleMap.cs
+++ punku/PerlinNoise/RollingParticleMap.cs
@@ -7,10 +7,8 @@ namespace Punku
 {
     public static class MyExtensions
     {    
-        public static void Shuffle<T> (this IList<T> list)
+        public static void Shuffle<T> (this IList<T> list, Random random)
         {              
-            Random random = new Random ();  
-
             int n = list.Count;  
 
             while (n > 1) {  
@@ -44,22 +42,22 @@ namespace Punku
         {
             byte[] map = new byte[height * width];
 
-            int edge_bias = 10;
+            int edge_bias = 12;
 
             int iterations = 0;
-            while (iterations++ < 600) {
+            while (iterations++ < 9000) {
 
                 // pick a random start particle near center
                 int y = random.Next (edge_bias, height - edge_bias - 1);                
                 int x = random.Next (edge_bias, width - edge_bias - 1);
 
-                // map [(y * height) + x] ++;
-
+                //   map [(y * width) + x] ++;
+        
 
                 // draw 50 times for each particle, color is increased when painting.
                 int particle_lifetime = 0;
                 while (particle_lifetime++ < 50) {
-                    byte c = map [(y * height) + x];
+                    byte c = map [(y * width) + x];
 
                     if ((x < 1 || x >= width - 1) || (y < 1 || y >= height - 1) || c == 255) {
                         // Log ("killed of particle at iteration " + iteration);
@@ -69,27 +67,42 @@ namespace Punku
                     // välj alla 3x3 runtomkring mig, blanda dem och börja från början 
                     // tills jag hittar en som är lägre färg än aktuella och hoppa till den positionen
                     var offsets = GetNeighbours3x3 (map, x, y, width, height);
+
+                    bool found = false;
                    
                     for (int i = 0; i < offsets.Count; i++) {
-                        byte new_c = map [(offsets[i].y * height) + offsets[i].x];
-                        if (new_c < c) {
+                        byte new_c = map [(offsets[i].y * width) + offsets[i].x];
+                        if (new_c <= c) {
                             // Log ("walking from " + x + "," + y + " + to " + offsets [i].x + ", " + offsets [i].y);
                             x = offsets [i].x;
                             y = offsets [i].y;
+                            found = true;
                             break;
                         }
+                    }
+                    if (!found) {
+                        //Log ("no lower point found, killing particle at iteration " + iterations);
+                        break;
                     }
 
                     // Log ("setting " + x + ", " + y + " to " + c);
 
-                    map [(y * height) + x]++;
+                    map [(y * width) + x]++;
                 }
-
             }
 
             map = NormalizeData (map, 0, 255);
 
             return ToImage (map, width, height);
+        }
+
+        static void PrintHexMap (byte[] map, int width, int height)
+        {
+            for (int y = 0; y < height; y++) {
+                byte[] second = new byte[width];            
+                Buffer.BlockCopy (map, y * width, second, 0, width);
+                Log (ByteArrayPrinter.ToHexString (second));
+            }
         }
         // return 3x3 (up to 8) valid coordinates around specified position
         private static List<Coordinate> GetNeighbours3x3 (byte[] data, int x, int y, int width, int height)
@@ -106,7 +119,9 @@ namespace Punku
                 }
             }
 
-            res.Shuffle ();
+            Random random = new Random ((int)DateTime.Now.Ticks);  
+
+            res.Shuffle (random);
 
             return res;
         }
@@ -133,7 +148,7 @@ namespace Punku
 
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
-                    byte value = map [(y * height) + x];
+                    byte value = map [(y * width) + x];
 
                     Color color = Color.FromArgb (value, value, value);
 
