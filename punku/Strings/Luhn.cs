e0d15168bd8fcce48f0f9c9379f883d0df103a76
Martin Lindhe <martin@ubique.se>
2013-12-18 22:34:29 +0100

    start work on port of SsnSwedish from core_dev

--- punku/Strings/Luhn.cs
+++ punku/Strings/Luhn.cs
@@ -2,8 +2,10 @@
  * To calculate the checksum, multiply the individual digits in the identity number with 212121-212.
  * The resulting products (a two digit product, such as 16, would be converted to 1 + 6) are
  * added together. The checksum is 10 minus the ones place digit in this sum.
+ * 
+ * http://en.wikipedia.org/wiki/Luhn_algorithm
  *
- * @author Martin Lindhe, 2007-2013 <martin@startwars.org>
+ * @author Martin Lindhe, 2007-2013 <martin@ubique.se>
  */ 
 using System;
 
@@ -16,29 +18,23 @@ namespace Punku.Strings
 			int sum = 0, cnt = 0;
 
 			for (int i = s.Length - 1; i >= 0; i--) {
+			
+				string c = s.Substring (i, 1);
+
 				// Switch between 212121212
-				string xx = s.Substring (i, 1);
-				var tmp = System.Convert.ToInt32 (xx, 10) * (2 - (cnt & 1));
+				int mod = (2 - (cnt & 1));
+				int tmp = System.Convert.ToInt32 (c, 10) * mod;
 
 				if (tmp > 9)
 					tmp -= 9;
 
-				//Log (xx + " * " + (2 - (cnt & 1)) + "  = " + tmp);
-
 				cnt++;
 				sum += tmp;
-
-				//Log (" ( sum = " + sum + ")");
 			}
 
 			// Substract the ones place digit from 10
 			sum = (10 - (sum % 10)) % 10;
 			return sum;
-		}
-
-		static void Log (string s)
-		{
-			Console.WriteLine ("[Luhn ]" + s);
 		}
 	}
 }
---
+++ punku/Validate/PersonalIdentityNumberSweden.cs
@@ -0,0 +1,60 @@
+﻿/**
+ * Handle swedish personal identity number (aka Personnummer)
+ *
+ * The personal identity number consists of 10 digits and a hyphen.
+ * The first six correspond to the person's birthday, in YYMMDD form.
+ * They are followed by a hyphen. The seventh through ninth are a serial number.
+ * An odd number is assigned to men, an even number to women.
+ * The tenth digit is a checksum.
+ *
+ * http://en.wikipedia.org/wiki/Personal_identity_number_%28Sweden%29
+ * http://sv.wikipedia.org/wiki/Organisationsnummer
+ *
+ * @author Martin Lindhe, 2007-2013 <martin@ubique.se>
+ */
+
+using System;
+
+// TODO acceptera "YYMMDD-XXXX" input. acceptera "YYYYMMDD-XXXX" åxå
+// TODO bool IsValid()-verifiera YYMMDD & base check, IsMale(), IsFemale(). ToDatestamp()
+namespace Punku
+{
+	public class OrganizationNumberSweden
+	{
+		public static bool IsValid (string s)
+		{
+			if (s.Length != 10)
+				return false;
+
+			int calc = Punku.Strings.Luhn.Calculate (s.Substring (0, 9));
+			int input = System.Convert.ToInt32 (s.Substring (s.Length - 1, 1), 10);
+
+			if (calc == input)
+				return true;
+
+			return false;
+		}
+
+		public static bool IsAktieBolag (string s)
+		{
+			if (!IsValid (s))
+				return false;
+
+			if (s.Substring (0, 2) != "55")
+				return false;
+
+			if (System.Convert.ToInt32 (s.Substring (2, 2), 10) < 20)
+				return false;
+
+			if (System.Convert.ToInt32 (s.Substring (4, 2), 10) < 20)
+				return false;
+
+			return true;
+		}
+	}
+
+	public class PersonalIdentityNumberSweden
+	{
+	}
+}
+
