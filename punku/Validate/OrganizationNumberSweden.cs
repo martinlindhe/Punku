09372f7ccd23514e69b75e64cd5e1913a89a920f
Martin Lindhe <martin@ubique.se>
2013-12-18 23:41:06 +0100

    PersonalIdentityNumberSweden: add IsFemale(), IsMale()

---
+++ punku/Validate/OrganizationNumberSweden.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * http://sv.wikipedia.org/wiki/Organisationsnummer
+ *
+ * @author Martin Lindhe, 2007-2013 <martin@ubique.se>
+ */
+using System;
+
+namespace Punku
+{
+	public class OrganizationNumberSweden
+	{
+		public static bool IsValid (string s)
+		{
+			s = s.Replace ("-", "");
+
+			if (s.Length == 12) {
+				var y = s.Substring (0, 2);
+				if (y == "19" || y == "20")
+					s = s.Substring (2);
+			}
+
+			if (s.Length != 10)
+				return false;
+
+			int calc = Punku.Strings.Luhn.Calculate (s.Substring (0, 9));
+			int input = System.Convert.ToInt32 (s.Substring (s.Length - 1, 1), 10);
+
+			if (calc == input)
+				return true;
+
+			return false;
+		}
+
+		public static bool IsAktieBolag (string s)
+		{
+			if (!IsValid (s))
+				return false;
+
+			if (s.Substring (0, 2) != "55")
+				return false;
+
+			if (System.Convert.ToInt32 (s.Substring (2, 2), 10) < 20)
+				return false;
+
+			if (System.Convert.ToInt32 (s.Substring (4, 2), 10) < 20)
+				return false;
+
+			return true;
+		}
+	}
+}
+
--- punku/Validate/PersonalIdentityNumberSweden.cs
+++ punku/Validate/PersonalIdentityNumberSweden.cs
@@ -4,27 +4,89 @@
  * The personal identity number consists of 10 digits and a hyphen.
  * The first six correspond to the person's birthday, in YYMMDD form.
  * They are followed by a hyphen. The seventh through ninth are a serial number.
- * An odd number is assigned to men, an even number to women.
+ * An odd eight number is assigned to men, an even number to women.
  * The tenth digit is a checksum.
  *
  * http://en.wikipedia.org/wiki/Personal_identity_number_%28Sweden%29
- * http://sv.wikipedia.org/wiki/Organisationsnummer
  *
  * @author Martin Lindhe, 2007-2013 <martin@ubique.se>
  */
 
 using System;
 
-// TODO acceptera "YYMMDD-XXXX" input. acceptera "YYYYMMDD-XXXX" åxå
-// TODO bool IsValid()-verifiera YYMMDD & base check, IsMale(), IsFemale(). ToDatestamp()
+// TODO ToDatestamp()
 namespace Punku
 {
-	public class OrganizationNumberSweden
+	public class PersonalIdentityNumberSweden
 	{
+		/**
+		 * odd = male
+		 */
+		public static bool IsMale (string s)
+		{
+			if (!IsValid (s))
+				return false;
+
+			var gender = System.Convert.ToInt32 (s.Substring (s.Length - 2, 1), 10);
+		
+			if ((gender % 2) != 0)
+				return true;
+
+			return false;
+		}
+
+		/**
+		 * even = female
+	     */
+		public static bool IsFemale (string s)
+		{
+			if (!IsValid (s))
+				return false;
+
+			var gender = System.Convert.ToInt32 (s.Substring (s.Length - 2, 1), 10);
+
+			if ((gender % 2) == 0)
+				return true;
+
+			return false;
+		}
+
 		public static bool IsValid (string s)
 		{
-			if (s.Length != 10)
+			s = s.Replace ("-", "");
+
+			int yy;
+
+			if (s.Length == 12) {
+				yy = System.Convert.ToInt32 (s.Substring (0, 4), 10);
+
+				var y2 = s.Substring (0, 2);
+				if (y2 == "19" || y2 == "20")
+					s = s.Substring (2);
+			} else if (s.Length == 10) {
+				yy = System.Convert.ToInt32 (s.Substring (0, 2), 10);
+
+				var now_last2 = System.Convert.ToInt32 (DateTime.Now.ToString ("yy"), 10);
+
+				if (yy > now_last2)
+					yy = 1900 + yy;
+				else
+					yy = 2000 + yy;
+
+			} else {
 				return false;
+			}
+
+			int mm = System.Convert.ToInt32 (s.Substring (2, 2), 10);
+			int dd = System.Convert.ToInt32 (s.Substring (4, 2), 10);
+			// Console.WriteLine ("yy = " + yy + ", mm = " + mm + ", dd = " + dd);
+
+			// fail = date is invalid
+			try {
+				DateTime testDate = new DateTime (yy, mm, dd);
+			} catch {
+				return false;
+			}
 
 			int calc = Punku.Strings.Luhn.Calculate (s.Substring (0, 9));
 			int input = System.Convert.ToInt32 (s.Substring (s.Length - 1, 1), 10);
@@ -34,27 +96,6 @@ namespace Punku
 
 			return false;
 		}
-
-		public static bool IsAktieBolag (string s)
-		{
-			if (!IsValid (s))
-				return false;
-
-			if (s.Substring (0, 2) != "55")
-				return false;
-
-			if (System.Convert.ToInt32 (s.Substring (2, 2), 10) < 20)
-				return false;
-
-			if (System.Convert.ToInt32 (s.Substring (4, 2), 10) < 20)
-				return false;
-
-			return true;
-		}
-	}
-
-	public class PersonalIdentityNumberSweden
-	{
 	}
 }
 
