b399781ff531f90d722d7d0e833f1a3383d0e0c9
Martin Lindhe <martin@ubique.se>
2013-12-27 13:00:48 +0100

    add Shift test

---
+++ PunkuTests/Strings/Shift.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Strings")]
+public class Strings_Shift
+{
+	[Test]
+	public void Test01 ()
+	{
+		Assert.AreEqual (Punku.Strings.Shift.ShiftLetters ("qrpgle.kyicrpylq()", 2), "string.maketrans()");
+	}
+
+	[Test]
+	public void Test02 ()
+	{
+		Assert.AreEqual (Punku.Strings.Shift.ShiftLetters ("rpylqjyrc", 2), "translate");
+	}
+}
+
