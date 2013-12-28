e95fa2d8d5d966cbf1cec7898b258c64c2a86e0b
Martin Lindhe <martin@ubique.se>
2013-12-28 15:09:02 +0100

    update test

--- PunkuTests/Strings/Shift.cs
+++ PunkuTests/Strings/Shift.cs
@@ -9,13 +9,13 @@ public class Strings_Shift
 	[Test]
 	public void Test01 ()
 	{
-		Assert.AreEqual (Punku.Strings.Shift.ShiftLetters ("qrpgle.kyicrpylq()", 2), "string.maketrans()");
+		Assert.AreEqual (Punku.Strings.Shift.ShiftString ("qrpgle.kyicrpylq()", 2), "string.maketrans()");
 	}
 
 	[Test]
 	public void Test02 ()
 	{
-		Assert.AreEqual (Punku.Strings.Shift.ShiftLetters ("rpylqjyrc", 2), "translate");
+		Assert.AreEqual (Punku.Strings.Shift.ShiftString ("rpylqjyrc", 2), "translate");
 	}
 }
 
