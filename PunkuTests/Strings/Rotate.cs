06e46ea9463facda1d5b3aea15c5c275e905b23d
Martin Lindhe <martin@ubique.se>
2013-12-28 18:06:08 +0100

    Rotate: merge with tests from Shift class, drop Shift test

--- PunkuTests/Strings/Rotate.cs
+++ PunkuTests/Strings/Rotate.cs
@@ -9,12 +9,48 @@ public class Strings_Rotate
 	[Test]
 	public void Test01 ()
 	{
-		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("secret"), "frperg");
+		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("abcdefghijklmnopqrstuvwxyz"), "nopqrstuvwxyzabcdefghijklm");
 	}
 
 	[Test]
 	public void Test02 ()
 	{
-		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("pnrfne fnynq"), "caesar salad");
+		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("nopqrstuvwxyzabcdefghijklm"), "abcdefghijklmnopqrstuvwxyz");
+	}
+
+	[Test]
+	public void Test03 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), "NOPQRSTUVWXYZABCDEFGHIJKLM");
+	}
+
+	[Test]
+	public void Test04 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.Rot13 ("NOPQRSTUVWXYZABCDEFGHIJKLM"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+	}
+
+	[Test]
+	public void Test05 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.RotateString ("abcdefghijklmnopqrstuvwxyz", 1), "bcdefghijklmnopqrstuvwxyza");
+	}
+
+	[Test]
+	public void Test06 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.RotateString ("abcdefghijklmnopqrstuvwxyz", 2), "cdefghijklmnopqrstuvwxyzab");
+	}
+
+	[Test]
+	public void Test07 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.RotateString ("abcdefghijklmnopqrstuvwxyz", 3), "defghijklmnopqrstuvwxyzabc");
+	}
+
+	[Test]
+	public void Test08 ()
+	{
+		Assert.AreEqual (Punku.Strings.Rotate.RotateString ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", 2), "CDEFGHIJKLMNOPQRSTUVWXYZAB");
 	}
 }
