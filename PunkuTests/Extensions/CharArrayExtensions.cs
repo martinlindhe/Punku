26f248b23f6de24a7249552ce39b05cfa4fcff1e
Martin Lindhe <martin@ubique.se>
2013-12-30 21:28:25 +0100

    add a test for ToUtf16String()

---
+++ PunkuTests/Extensions/CharArrayExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Extensions")]
+public class Extensions_CharArray
+{
+	[Test]
+	public void ToUtf16String01 ()
+	{
+		string s1 = "hello";
+		string s2 = s1.ToCharArray ().ToUtf16String ();
+
+		Assert.AreEqual (s1, s2);
+	}
+}
