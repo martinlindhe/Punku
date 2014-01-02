9d94e076ba5ccc2fd0e19cc004ce5e59f99d0613
Martin Lindhe <martin@ubique.se>
2014-01-02 23:32:33 +0100

    Add Sha2_384
    autoformat BinaryReader.cs

--- punku/IFileReader/BinaryReader.cs
+++ punku/IFileReader/BinaryReader.cs
@@ -3,47 +3,42 @@ using System.IO;
 
 namespace Punku
 {
-    public class BinaryReader : IFileReader
-    {
-        public byte[] Data { get; private set; }
+	public class BinaryReader : IFileReader
+	{
+		public byte[] Data { get; private set; }
 
-        public long Length { get; private set; }
+		public long Length { get; private set; }
 
-        public BinaryReader(string filename)
-        {
-            Read(filename);
-        }
+		public BinaryReader (string filename)
+		{
+			Read (filename);
+		}
 
-        public void Read(string filename)
-        {            
-            try
-            {
-                var src = new FileStream(filename, FileMode.Open, FileAccess.Read);
-
-                this.Length = src.Length;
-                this.Data = new byte[this.Length];
-                             
-                var numBytesToRead = this.Length;            
-                var numBytesRead = 0;
-
-                // TODO redo to a  do-while loop when test case first exist
-               
-                while (numBytesToRead > 0)
-                {                   
-                    int n = src.Read(this.Data, numBytesRead, (int)numBytesToRead);
-
-                    if (n == 0)
-                        break;
-
-                    numBytesRead += n;
-                    numBytesToRead -= n;
-                }
+		/**
+		 * NOTE: src.Read() can throw FileNotFoundException
+		 */
+		public void Read (string filename)
+		{            
             
-            }
-            catch (FileNotFoundException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-        }
-    }
+			var src = new FileStream (filename, FileMode.Open, FileAccess.Read);
+
+			this.Length = src.Length;
+			this.Data = new byte[this.Length];
+                             
+			var numBytesToRead = this.Length;            
+			var numBytesRead = 0;
+
+			// TODO redo to a  do-while loop when test case first exist
+               
+			while (numBytesToRead > 0) {                   
+				int n = src.Read (this.Data, numBytesRead, (int)numBytesToRead);
+
+				if (n == 0)
+					break;
+
+				numBytesRead += n;
+				numBytesToRead -= n;
+			}
+		}
+	}
 }
