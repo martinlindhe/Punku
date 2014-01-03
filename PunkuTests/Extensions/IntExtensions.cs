772ffe004e0a55306e2a4fbea9f61bd927c3fe81
Martin Lindhe <martin@ubique.se>
2014-01-03 17:05:53 +0100

    add tests for int extension IsNegative()

---
+++ PunkuTests/Extensions/IntExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Extensions")]
+public class Extensions_Int
+{
+	[Test]
+	public void Negative01 ()
+	{
+		int x = 1;
+		Assert.AreEqual (x.IsNegative (), false);
+	}
+
+	[Test]
+	public void Negative02 ()
+	{
+		int x = 0;
+		Assert.AreEqual (x.IsNegative (), false);
+	}
+
+	[Test]
+	public void Negative03 ()
+	{
+		int x = -1;
+		Assert.AreEqual (x.IsNegative (), true);
+	}
+}
