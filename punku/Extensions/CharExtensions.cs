263955f9556367eeeea2ab173377eb1d71bb9bbf
Martin Lindhe <martin@ubique.se>
2014-01-03 17:46:02 +0100

    add char extension ToHexString()

---
+++ punku/Extensions/CharExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+public static class CharExtensions
+{
+	/**
+	 * Returns a 4-letter long hex value representation of char (UTF-16)
+	 */
+	public static string ToHexString (this char c)
+	{
+		int value = Convert.ToInt32 (c);
+		return value.ToString ("x4");
+	}
+}
+
