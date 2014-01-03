f7a62a4cbf201826fbe3a9e0584846242dbb454f
Martin Lindhe <martin@ubique.se>
2014-01-03 17:46:31 +0100

    add char[] extension ToHexString()

--- punku/Extensions/CharArrayExtensions.cs
+++ punku/Extensions/CharArrayExtensions.cs
@@ -3,9 +3,28 @@ using System.Text;
 
 public static class CharArrayExtensions
 {
-	public static string ToUtf16String (this char[] input)
+	/**
+	 * Returns a hex string representation of the content,
+	 * where each char is 4 hex letters (UTF-16)
+	 */
+	public static string ToHexString (this char[] input)
 	{
 		var res = new StringBuilder ();
+
+		foreach (char c in input)
+			res.Append (c.ToHexString ());
+
+		return res.ToString ();
+	}
+
+	/**
+	 * Returns a UTF-16 string representation of the char[]
+	 */
+	public static string ToUtf16String (this char[] input)
+	{
+		// TODO use encoding.convert ?!
+		var res = new StringBuilder ();
+
 		foreach (char c in input)
 			res.Append (c);
 	
