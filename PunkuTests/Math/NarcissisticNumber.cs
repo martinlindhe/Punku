a8c74db621a409ccbc67690b1f5e0e005de5facf
Martin Lindhe <martin@ubique.se>
2014-01-08 20:36:03 +0100

    rename test

--- PunkuTests/Math/NarcissisticNumber.cs
+++ PunkuTests/Math/NarcissisticNumber.cs
@@ -4,26 +4,26 @@ using Punku;
 
 [TestFixture]
 [Category ("Math")]
-public class Math_Tests
+public class Math_NarcissisticNumber
 {
 	[Test]
 	public void IsNarcissisticNumber01 ()
 	{
 		ulong x = 123;
-		Assert.AreEqual (Punku.Math.IsNarcissisticNumber (x), false);
+		Assert.AreEqual (Punku.NarcissisticNumber.IsNarcissisticNumber (x), false);
 	}
 
 	[Test]
 	public void IsNarcissisticNumber02 ()
 	{
 		ulong x = 8208;
-		Assert.AreEqual (Punku.Math.IsNarcissisticNumber (x), true);
+		Assert.AreEqual (Punku.NarcissisticNumber.IsNarcissisticNumber (x), true);
 	}
 
 	[Test]
 	public void IsNarcissisticNumber03 ()
 	{
 		ulong x = 32164049651;
-		Assert.AreEqual (Punku.Math.IsNarcissisticNumber (x), true);
+		Assert.AreEqual (Punku.NarcissisticNumber.IsNarcissisticNumber (x), true);
 	}
 }
