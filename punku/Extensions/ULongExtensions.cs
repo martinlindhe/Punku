d593f2a4dfec3f3853447a7f92b89179ae4f984d
Martin Lindhe <martin@ubique.se>
2014-01-08 20:35:52 +0100

    move narcisstic number to own self contained class, drop Math.cs for now

--- punku/Extensions/ULongExtensions.cs
+++ punku/Extensions/ULongExtensions.cs
@@ -6,13 +6,13 @@ public static class ULongExtensions
 	/**
 	 * @return the number of digits in the number
 	 */
-	public static uint CountDigits (this ulong i, uint digitBase = 10)
+	public static uint CountDigits (this ulong i, uint numberBase = 10)
 	{
 		uint cnt = 1;
 
-		while (i >= digitBase) {
+		while (i >= numberBase) {
 			cnt++;
-			i /= digitBase;
+			i /= numberBase;
 		}
 
 		return cnt;
@@ -21,14 +21,14 @@ public static class ULongExtensions
 	/**
 	 * @return byte array of the separate digits in the number (base 10)
 	 */
-	public static byte[] Digits (this ulong i, uint digitBase = 10)
+	public static byte[] Digits (this ulong i, uint numberBase = 10)
 	{
-		int count = (int)i.CountDigits (digitBase);
+		int count = (int)i.CountDigits (numberBase);
 		byte[] digits = new byte[count];
 
 		for (int n = count - 1; n >= 0; n--) {
-			digits [n] = (byte)(i % digitBase);
-			i /= digitBase;
+			digits [n] = (byte)(i % numberBase);
+			i /= numberBase;
 		}
 
 		return digits;
@@ -37,18 +37,18 @@ public static class ULongExtensions
 	/**
 	 * @return string representation of i written in base digitBase
 	 */
-	public static string ToBase (this ulong i, uint digitBase)
+	public static string ToBase (this ulong i, uint numberBase)
 	{
 		string AlphaCodes = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-		if (digitBase < 2 || digitBase > AlphaCodes.Length)
+		if (numberBase < 2 || numberBase > AlphaCodes.Length)
 			throw new ArgumentException ("digitBase");
 
 		var sb = new StringBuilder ();
 
 		while (i > 0) {
-			sb.Insert (0, AlphaCodes [(int)(i % digitBase)]);
-			i /= digitBase;
+			sb.Insert (0, AlphaCodes [(int)(i % numberBase)]);
+			i /= numberBase;
 		}
 
 		return sb.ToString ();
--- punku/Math/NarcissisticNumber.cs
+++ punku/Math/NarcissisticNumber.cs
@@ -1,4 +1,9 @@
-﻿// toy
+﻿/**
+ * toy
+ * 
+ * https://en.wikipedia.org/wiki/Narcissistic_number
+ * http://oeis.org/A005188
+ */
 using System;
 using Punku;
 
@@ -15,12 +20,31 @@ namespace Punku
 
 			for (ulong i = 1; i < ulong.MaxValue; i++) {
 
-				if (Punku.Math.IsNarcissisticNumber (i, numberBase)) {
+				if (IsNarcissisticNumber (i, numberBase)) {
 					Console.WriteLine ("nars in base " + numberBase + " = " + Punku.Convert.Base.ToBase (i, numberBase));
 				}
 			}
 
 			time.Stop ();
+		}
+
+		/**
+		 * Checks if n is a narcissistic number
+		 */
+		public static bool IsNarcissisticNumber (ulong n, uint numberBase = 10)
+		{
+			// NOTE: there can be a rounding error due to Math.Pow using doubles
+
+			ulong res = 0;
+			uint digitCount = n.CountDigits (numberBase);
+
+			foreach (var digit in n.Digits (numberBase))
+				res += (ulong)System.Math.Pow (digit, digitCount);
+
+			if (n == res)
+				return true;
+
+			return false;
 		}
 	}
 }
