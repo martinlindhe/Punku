3828e4b8735b494e7384f83b34816a15afdf6cf3
Martin Lindhe <martin@ubique.se>
2014-01-08 11:11:03 +0100

    add Digits() tests

--- PunkuTests/Extensions/IntExtensions.cs
+++ PunkuTests/Extensions/IntExtensions.cs
@@ -28,30 +28,50 @@ public class Extensions_Int
 	}
 
 	[Test]
-	public void GetDigitCount01 ()
+	public void CountDigits01 ()
 	{
 		int x = 1;
 		Assert.AreEqual (x.CountDigits (), 1);
 	}
 
 	[Test]
-	public void GetDigitCount02 ()
+	public void CountDigits02 ()
 	{
 		int x = 1234;
 		Assert.AreEqual (x.CountDigits (), 4);
 	}
 
 	[Test]
-	public void GetDigitCount03 ()
+	public void CountDigits03 ()
 	{
 		int x = 99999999;
 		Assert.AreEqual (x.CountDigits (), 8);
 	}
 
 	[Test]
-	public void GetDigitCount04 ()
+	public void CountDigits04 ()
 	{
 		int x = 123456789;
 		Assert.AreEqual (x.CountDigits (), 9);
+	}
+
+	[Test]
+	public void Digits01 ()
+	{
+		int x = 7;
+		Assert.AreEqual (
+			x.Digits (),
+			new byte[] { 7 }
+		);
+	}
+
+	[Test]
+	public void Digits02 ()
+	{
+		int x = 123456789;
+		Assert.AreEqual (
+			x.Digits (),
+			new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+		);
 	}
 }
