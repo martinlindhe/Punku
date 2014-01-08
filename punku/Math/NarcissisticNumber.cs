1e1c94865ea784ee2be5527b287ffc6fcdd4a3e8
Martin Lindhe <martin@ubique.se>
2014-01-08 20:32:46 +0100

    add a toy class for narcissist numbers

---
+++ punku/Math/NarcissisticNumber.cs
@@ -0,0 +1,27 @@
+﻿// toy
+using System;
+using Punku;
+
+namespace Punku
+{
+	public class NarcissisticNumber
+	{
+		/**
+		 * narcissistic number finder
+		 */
+		public static void Finder (uint numberBase)
+		{
+			var time = new MeasureTime ();
+
+			for (ulong i = 1; i < ulong.MaxValue; i++) {
+
+				if (Punku.Math.IsNarcissisticNumber (i, numberBase)) {
+					Console.WriteLine ("nars in base " + numberBase + " = " + Punku.Convert.Base.ToBase (i, numberBase));
+				}
+			}
+
+			time.Stop ();
+		}
+	}
+}
+
