cd2ea095c85f2994524de05cb83b870b996d7044
Martin Lindhe <martin@ubique.se>
2014-01-08 11:38:41 +0100

    add Math.IsNarscissticNumber() tests

---
+++ PunkuTests/Math/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Math")]
+public class Math_Tests
+{
+	[Test]
+	public void IsNarcissisticNumber01 ()
+	{
+		int x = 123;
+		Assert.AreEqual (Punku.Math.IsNarcissisticNumber (x), false);
+	}
+
+	[Test]
+	public void IsNarcissisticNumber02 ()
+	{
+		int x = 8208;
+		Assert.AreEqual (Punku.Math.IsNarcissisticNumber (x), true);
+	}
+}
