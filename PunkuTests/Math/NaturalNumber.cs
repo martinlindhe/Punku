cedf34e1564f376d016a36b9a021bdfb8a014ea0
Martin Lindhe <martin@ubique.se>
2014-01-09 17:05:02 +0100

    add more NaturalNumber tests

--- PunkuTests/Math/NaturalNumber.cs
+++ PunkuTests/Math/NaturalNumber.cs
@@ -19,4 +19,30 @@ public class Math_NaturalNumber
 		var bb = new NaturalNumber ("00012345");
 		Assert.AreEqual (bb.ToDecimal (), 12345);
 	}
+
+	[Test]
+	public void ToDecimal03 ()
+	{
+		// NOTE: biggest number that fits in decimal datatype
+		var bb = new NaturalNumber ("79228162514264337593543950335");
+		Assert.AreEqual (bb.ToDecimal (), 79228162514264337593543950335m);
+	}
+
+	[Test]
+	[ExpectedException (typeof(OverflowException))]
+	public void ToDecimal04 ()
+	{
+		var bb = new NaturalNumber ("79228162514264337593543950336");
+		bb.ToDecimal ();
+	}
+
+	[Test]
+	public void Verify01 ()
+	{
+		var bb = new NaturalNumber ("123");
+		Assert.AreEqual (
+			bb.Digits, 
+			new byte[] { 1, 2, 3 }
+		);
+	}
 }
