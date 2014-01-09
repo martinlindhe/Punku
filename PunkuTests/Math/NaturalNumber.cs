7ca5297edd5b6f40b98332731d22f0337fda9a09
Martin Lindhe <martin@ubique.se>
2014-01-09 16:58:54 +0100

    add some NaturalNumber tests

---
+++ PunkuTests/Math/NaturalNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Math")]
+public class Math_NaturalNumber
+{
+	[Test]
+	public void ToDecimal01 ()
+	{
+		var bb = new NaturalNumber ("12345");
+		Assert.AreEqual (bb.ToDecimal (), 12345);
+	}
+
+	[Test]
+	public void ToDecimal02 ()
+	{
+		var bb = new NaturalNumber ("00012345");
+		Assert.AreEqual (bb.ToDecimal (), 12345);
+	}
+}
