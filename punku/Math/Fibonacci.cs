1d9afc1b3673d56c54a05a65bef325975009b0e4
Martin Lindhe <martin@ubique.se>
2014-01-11 14:28:13 +0100

    add Fibonacci sequence tool

---
+++ punku/Math/Fibonacci.cs
@@ -0,0 +1,24 @@
+﻿/**
+ * toy
+ */
+using System;
+
+namespace Punku
+{
+	public class Fibonacci
+	{
+		public static void PrintFibonacci ()
+		{ 
+			ulong fib = 1;
+			ulong[] hold = new ulong[2];
+
+			for (int i = 0; i < 10; i++) {
+				hold [0] = hold [1];
+				hold [1] = fib;
+				fib = hold [0] + hold [1];
+				Console.WriteLine (hold [0] + " + " + hold [1] + " = " + fib);
+			} 
+		}
+	}
+}
+
