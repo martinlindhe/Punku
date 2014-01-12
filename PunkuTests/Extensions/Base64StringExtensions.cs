ebd78240f6c0d212079dd1f188a66b1c8bca8cce
Martin Lindhe <martin@ubique.se>
2014-01-12 14:33:33 +0100

    cleanup

--- PunkuTests/Extensions/Base64StringExtensions.cs
+++ PunkuTests/Extensions/Base64StringExtensions.cs
@@ -10,10 +10,8 @@ public class Extensions_Base64String
 	[Test]
 	public void ByteArrayToBase64_01 ()
 	{
-		byte[] x = { 1, 2, 3 };
-
 		Assert.AreEqual (
-			x.ToBase64 (),
+			new byte[] { 1, 2, 3 }.ToBase64 (),
 			"AQID"
 		);
 	}
@@ -21,13 +19,18 @@ public class Extensions_Base64String
 	[Test]
 	public void StringToBase64_01 ()
 	{
-		Assert.AreEqual ("pleasure.".ToBase64 (), "cGxlYXN1cmUu");
+		Assert.AreEqual (
+			"pleasure.".ToBase64 (),
+			"cGxlYXN1cmUu"
+		);
 	}
 
 	[Test]
 	public void StringFromBase64_01 ()
 	{
-		Assert.AreEqual ("aGVsbG8=".FromBase64 (), "hello");
+		Assert.AreEqual (
+			"aGVsbG8=".FromBase64 (),
+			"hello");
 	}
 
 	[Test]
