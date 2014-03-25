6bd8ec99b426126600c64f4f7e49e5e5c19bdb8c
Martin Lindhe <martin@ubique.se>
2014-03-25 17:50:08 +0100

    add IPv4 as uint32 to/from methods + tests

---
+++ Punku/Network/IPv4.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+// NOTE: IPAddress is a internal C# class
+namespace Punku
+{
+	public class IPv4
+	{
+		/**
+		 * Converts input string containing a IPv4 number to a uint32
+		 */
+		public static uint ToUInt32 (string s)
+		{
+			string[] parts = s.Split ('.');
+			if (parts.Length != 4)
+				throw new FormatException ();
+
+			long res = 0;
+			long multiplier = 0x1000000;
+
+			foreach (var x in parts) {
+				int val;
+				if (int.TryParse (x, out val) == false)
+					throw new FormatException ();
+
+				if (val > 255)
+					throw new FormatException ();
+
+				res += val * multiplier;
+				multiplier /= 256;
+			}
+
+			if (res > uint.MaxValue)
+				throw new FormatException ();
+
+			return (uint)res;
+		}
+
+		/**
+		 * Converts a uint to a IPv4 number as a string
+		 */
+		public static string ToString (uint value)
+		{
+			return Punku.Strings.DottedDecimalNotation.ToDecimalNotation (value, 8, '.');
+		}
+
+		/**
+		 * Returns true if input string is a valid IPv4 address
+		 */
+		public static bool IsIPv4 (string s)
+		{
+			try {
+				ToUInt32 (s);
+				return true;
+			} catch (FormatException) {
+				return false;
+			}
+		}
+	}
+}
+
---
+++ PunkuTests/Network/IPv4.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Network")]
+public class Network_IPv4
+{
+	[Test]
+	public void ToString01 ()
+	{
+		Assert.AreEqual (Punku.IPv4.ToString (0xFF), "0.0.0.255");
+	}
+
+	[Test]
+	public void ToString02 ()
+	{
+		Assert.AreEqual (Punku.IPv4.ToString (0xFFFF0000), "255.255.0.0");
+	}
+
+	[Test]
+	public void ToUInt01 ()
+	{
+		Assert.AreEqual (Punku.IPv4.ToUInt32 ("0.0.0.255"), 0xFF);
+	}
+
+	[Test]
+	public void ToUInt02 ()
+	{
+		Assert.AreEqual (Punku.IPv4.ToUInt32 ("255.255.0.0"), 0xFFFF0000);
+	}
+
+	[Test]
+	public void IsIPv4_01 ()
+	{
+		Assert.AreEqual (Punku.IPv4.IsIPv4 ("255.255.0.0"), true);
+	}
+
+	[Test]
+	public void IsIPv4_02 ()
+	{
+		Assert.AreEqual (Punku.IPv4.IsIPv4 ("255.255.0."), false);
+	}
+
+	[Test]
+	public void IsIPv4_03 ()
+	{
+		Assert.AreEqual (Punku.IPv4.IsIPv4 ("255.255.0"), false);
+	}
+
+	[Test]
+	public void IsIPv4_04 ()
+	{
+		Assert.AreEqual (Punku.IPv4.IsIPv4 ("255.255.255.255.0"), false);
+	}
+
+	[Test]
+	public void IsIPv4_05 ()
+	{
+		Assert.AreEqual (Punku.IPv4.IsIPv4 ("256.255.0.0"), false);
+	}
+}
