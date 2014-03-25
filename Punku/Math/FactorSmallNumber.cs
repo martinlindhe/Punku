ff65a5f2fad65dd2b7387fe8f03f3c6e4429b91f
Martin Lindhe <martin@ubique.se>
2014-03-25 18:18:13 +0100

    Math: add FactorSmallNumber class + tests

---
+++ Punku/Math/FactorSmallNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Punku
+{
+	public class FactorSmallNumber
+	{
+		public static List<long> Factor (long value)
+		{
+			var res = new List<long> ();
+
+			while (value > 1) {
+				var factor = 2;
+				if (value % factor != 0) {
+					// find next factor faster
+					factor += 1;
+					do {
+						if (value % factor == 0)
+							break;
+
+						factor += 2;
+					} while (factor < value);
+				}
+
+				value /= factor;
+				res.Add (factor);
+			}
+
+			return res;
+		}
+	}
+}
+
---
+++ PunkuTests/Math/FactorSmallNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+using System.Collections.Generic;
+
+[TestFixture]
+[Category ("Math")]
+public class Math_FactorSmallNumber
+{
+	[Test]
+	public void Factor01 ()
+	{
+		Assert.AreEqual (
+			Punku.FactorSmallNumber.Factor (362), 
+			new List<long> { 2, 181 }
+		);
+	}
+}
