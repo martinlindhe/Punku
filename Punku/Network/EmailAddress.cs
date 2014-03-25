42f4742a7c1e0a0dd2cb0e96c7a6ecd79a0598f6
Martin Lindhe <martin@ubique.se>
2014-03-25 19:22:20 +0100

    network: add EmailAddress.IsValid() & tests

---
+++ Punku/Network/EmailAddress.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Punku.Network
+{
+	public class EmailAddress
+	{
+		public static bool IsValid (string s)
+		{
+			// allows utf8: @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
+
+			return Regex.IsMatch (
+				s, 
+				@"^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$"
+			);
+		}
+	}
+}
+
---
+++ PunkuTests/Network/EmailAddress.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using Punku;
+
+[TestFixture]
+[Category ("Network")]
+public class Network_EmailAddress
+{
+	[Test]
+	public void IsValid01 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("a@test.com"), true);
+	}
+
+	[Test]
+	public void IsValid02 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("first.last@test.com"), true);
+	}
+
+	[Test]
+	public void IsValid03 ()
+	{
+		// NOTE: we dont allow utf-8 in email addresses
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("körv@test.com"), false);
+	}
+}
