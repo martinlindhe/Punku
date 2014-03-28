e5e2f2a9b5472686eee7b69504bc75c63973d498
Martin Lindhe <martin@ubique.se>
2014-03-28 18:53:28 +0100

    network: finished EmailAddress.IsValid(), all tests pass

--- Punku/Network/EmailAddress.cs
+++ Punku/Network/EmailAddress.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿// NOTE internal MailAddress class exists
+
+using System;
 using System.Text.RegularExpressions;
 
 namespace Punku.Network
 {
 	public class EmailAddress
 	{
+		/**
+		 * @return true if s is a properly formatted e-mail address
+		 *
+		 * NOTE: allows utf-8 addresses
+		 */
 		public static bool IsValid (string s)
 		{
-			// allows utf8: @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
-
 			return Regex.IsMatch (
 				s, 
-				@"^([a-zA-Z0-9])+([a-zA-Z0-9._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9._-]+)+$"
+				@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"
 			);
 		}
 	}
--- PunkuTests/Network/EmailAddress.cs
+++ PunkuTests/Network/EmailAddress.cs
@@ -7,21 +7,87 @@ using Punku;
 public class Network_EmailAddress
 {
 	[Test]
-	public void IsValid01 ()
+	public void Valid01 ()
 	{
 		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("a@test.com"), true);
 	}
 
 	[Test]
-	public void IsValid02 ()
+	public void Valid02 ()
 	{
 		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("first.last@test.com"), true);
 	}
 
 	[Test]
-	public void IsValid03 ()
+	public void Valid03 ()
 	{
-		// NOTE: we dont allow utf-8 in email addresses
-		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("körv@test.com"), false);
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("first-last@test.com"), true);
+	}
+
+	[Test]
+	public void Valid04 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("1@test.com"), true);
+	}
+
+	[Test]
+	public void Valid05 ()
+	{
+		// NOTE: we allow utf-8 in email addresses
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("körv@test.com"), true);
+	}
+
+	[Test]
+	public void Invalid01 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("a*@test.com"), false);
+	}
+
+	[Test]
+	public void Invalid02 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("a?@test.com"), false);
+	}
+
+	[Test]
+	public void Invalid03 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("a#@test.com"), false);
+	}
+
+	[Test]
+	public void Invalid04 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("first@last@test.com"), false);
+	}
+
+	[Test]
+	public void Invalid05 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("first@@test.com"), false);
+	}
+
+	[Test]
+	public void Invalid06 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("hej.hej.com"), false);
+	}
+
+	[Test]
+	public void Invalid07 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("@hej.hej.com"), false);
+	}
+
+	[Test]
+	public void Invalid08 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("hej@hej."), false);
+	}
+
+	[Test]
+	public void Invalid09 ()
+	{
+		Assert.AreEqual (Punku.Network.EmailAddress.IsValid ("hej@hej"), false);
 	}
 }
