8c13edab122cb93d92a4d99a0172e82342f64270
Martin Lindhe <martin@ubique.se>
2014-03-28 22:03:20 +0100

    strings: use the more common MinValue/MaxValue naming and make methods into properties

--- Punku/Strings/DiceDnD.cs
+++ Punku/Strings/DiceDnD.cs
@@ -6,13 +6,15 @@ using System;
 // TODO rework to a static class as it would fit usage better?
 namespace Punku.Game
 {
-    public class DiceDnD
-    {
-        protected int numberOfDices;
-        protected int numberOfDots;
-        protected int adjustment;
+	public class DiceDnD
+	{
+		protected int numberOfDices;
+		protected int numberOfDots;
+		public int MinValue;
+		public int MaxValue;
+		protected int adjustment;
 
-        /**
+		/**
          * @param cmd nDd([+/-]m)
          *
          * where
@@ -21,49 +23,42 @@ namespace Punku.Game
          * m - modifier
          * and optional modifier as a signed integer
          */
-        public DiceDnD (string cmd)
-        {
-            int pos = cmd.IndexOfAny (new char[] { 'D', 'd' });
-            if (pos < 0)
-                throw new ArgumentException ();
+		public DiceDnD (string cmd)
+		{
+			int pos = cmd.IndexOfAny (new char[] { 'D', 'd' });
+			if (pos < 0)
+				throw new ArgumentException ();
 
-            numberOfDices = System.Convert.ToInt32 (cmd.Substring (0, pos));  
-            adjustment = 0;
+			numberOfDices = System.Convert.ToInt32 (cmd.Substring (0, pos));  
+			adjustment = 0;
 
-            string s = cmd.Substring (pos + 1);
+			string s = cmd.Substring (pos + 1);
 
-            pos = s.IndexOf ('+');
-            if (pos < 0)
-                pos = s.IndexOf ('-');
+			pos = s.IndexOf ('+');
+			if (pos < 0)
+				pos = s.IndexOf ('-');
 
-            if (pos < 0) {
-                numberOfDots = System.Convert.ToInt32 (s);
-            } else {
-                numberOfDots = System.Convert.ToInt32 (s.Substring (0, pos));
-                adjustment = System.Convert.ToInt32 (s.Substring (pos));
-            }
-        }
+			if (pos < 0) {
+				numberOfDots = System.Convert.ToInt32 (s);
+			} else {
+				numberOfDots = System.Convert.ToInt32 (s.Substring (0, pos));
+				adjustment = System.Convert.ToInt32 (s.Substring (pos));
+			}
 
-        public int Roll ()
-        {
-            int result = 0;
+			MinValue = numberOfDices + adjustment;
+			MaxValue = (numberOfDices * numberOfDots) + adjustment;
+		}
 
-            Random random = new Random ();
+		public int Roll ()
+		{
+			int result = 0;
 
-            for (int i = 0; i < numberOfDices; i++)
-                result += random.Next (numberOfDots) + 1;
+			Random random = new Random ();
 
-            return result + adjustment;
-        }
+			for (int i = 0; i < numberOfDices; i++)
+				result += random.Next (numberOfDots) + 1;
 
-        public int SmallestPossible ()
-        {
-            return numberOfDices + adjustment;	
-        }
-
-        public int LargestPossible ()
-        {
-            return (numberOfDices * numberOfDots) + adjustment;
-        }
-    }
+			return result + adjustment;
+		}
+	}
 }
--- PunkuTests/Strings/DiceDnD.cs
+++ PunkuTests/Strings/DiceDnD.cs
@@ -8,16 +8,16 @@ public class Strings_DiceDnD
 {
 	private void RandomCheckRolls (Punku.Game.DiceDnD d)
 	{
-		Assert.LessOrEqual (d.SmallestPossible (), d.Roll ()); 
-		Assert.GreaterOrEqual (d.LargestPossible (), d.Roll ());
+		Assert.LessOrEqual (d.MinValue, d.Roll ()); 
+		Assert.GreaterOrEqual (d.MaxValue, d.Roll ());
 	}
 
 	[Test]
 	public void Parse01 ()
 	{
 		var d = new Punku.Game.DiceDnD ("2D6");
-		Assert.AreEqual (d.SmallestPossible (), 2, "FAIL 1"); 
-		Assert.AreEqual (d.LargestPossible (), 12, "FAIL 2"); 
+		Assert.AreEqual (d.MinValue, 2, "FAIL 1");
+		Assert.AreEqual (d.MaxValue, 12, "FAIL 2"); 
 		RandomCheckRolls (d);
 	}
 
@@ -25,8 +25,8 @@ public class Strings_DiceDnD
 	public void Parse02 ()
 	{
 		var d = new Punku.Game.DiceDnD ("1D8+60");
-		Assert.AreEqual (d.SmallestPossible (), 61, "FAIL 3"); 
-		Assert.AreEqual (d.LargestPossible (), 68, "FAIL 4"); 
+		Assert.AreEqual (d.MinValue, 61, "FAIL 3"); 
+		Assert.AreEqual (d.MaxValue, 68, "FAIL 4"); 
 		RandomCheckRolls (d);
 	}
 
@@ -34,8 +34,8 @@ public class Strings_DiceDnD
 	public void Parse03 ()
 	{
 		var d = new Punku.Game.DiceDnD ("10d2-10");
-		Assert.AreEqual (d.SmallestPossible (), 0, "FAIL 5"); 
-		Assert.AreEqual (d.LargestPossible (), 10, "FAIL 6"); 
+		Assert.AreEqual (d.MinValue, 0, "FAIL 5"); 
+		Assert.AreEqual (d.MaxValue, 10, "FAIL 6"); 
 		RandomCheckRolls (d);    
 	}
 
